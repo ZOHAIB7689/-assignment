bd8ad7e3dfa9b4f8a2df3da51807e09b8162f2f6
ZOHAIB7689 <159872246+ZOHAIB7689@users.noreply.github.com>
2024-03-10 01:07:10 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,368 @@
+// assignment by sir asharib
+// solve 10 questions of operators
+
+
+// {1}
+console.log(1)
+    {                    //  erthmatic-operators
+const  num1:number = 13;
+const  num2 :number = 65;
+const  addition  = num1 + num2;
+const  subtraction = num2 - num1;
+const  multiplication  = num1 * num2;
+const  division = num2 / num1;
+const  power = num1 ** 2;
+const  modulo = num2 % num1;
+const  equalto  = num1==num2;
+const  not_equalto  = num1 != num2;
+const  equal2  = num1 === num2;
+const  greaterThan = num1 > num2;
+const  lessThan = num1< num2;
+const  greater_or_eqalto = num1 >= num2;
+const  less_equal_than = num1 <=  num2;
+
+console.log( "13 + 65 =", addition );
+console.log( "65 - 13  =" , subtraction );
+console.log( "13 * 65 =", multiplication );
+console.log( "65 / 13 =",division);
+console.log( "13 power 2  =",power);
+console.log( "13 / 65 reminder =",modulo);
+console.log( "13 == 65 =" , equalto);
+console.log( "13 != 65 =" , not_equalto);
+console.log( "13 === 65 =" , equal2);
+console.log( "13 > 65 =", greaterThan);
+console.log( "13 < 65 =", lessThan);
+console.log( "13 >= 65 =", greater_or_eqalto);
+console.log( "13 <= 65 =", less_equal_than);
+}
+
+//  {02}
+
+console.log(2)
+{                    
+    const  num1:number = 6;
+    const  num2 :number = 34;
+    const  addition  = num1 + num2;
+    const  subtraction = num2 - num1;
+    const  multiplication  = num1 * num2;
+    const  division = num2 / num1;
+    const  power = num1 ** 2;
+    const  modulo = num2 % num1;
+    const  equalto  = num1==num2;
+    const  not_equalto  = num1 != num2;
+    const  equal2  = num1 === num2;
+    const  greaterThan = num1 > num2;
+    const  lessThan = num1< num2;
+    const  greater_or_eqalto = num1 >= num2;
+    const  less_equal_than = num1 <=  num2;
+    
+    console.log( "6 + 34  =", addition );
+    console.log( "34 - 6  =" , subtraction );
+    console.log( "6 * 34 =", multiplication );
+    console.log( "34 / 6 =",division);
+    console.log( "6 power 2  =",power);
+    console.log( "6 / 34 reminder =",modulo);
+    console.log( "34 == 6 =" , equalto);
+    console.log( "34 != 6 =" , not_equalto);
+    console.log( "6 === 34 =" , equal2);
+    console.log( "6 > 34 =", greaterThan);
+    console.log( "6 < 34 =", lessThan);
+    console.log( "6 >= 34 =", greater_or_eqalto);
+    console.log( "6 <= 34 =", less_equal_than);
+    }
+    
+    
+    // {3}
+
+    console.log(3)
+    {                    
+        const  num1:number = 78;
+        const  num2 :number = 9;
+        const  addition  = num1 + num2;
+        const  subtraction = num2 - num1;
+        const  multiplication  = num1 * num2;
+        const  division = num2 / num1;
+        const  power = num1 ** 2;
+        const  modulo = num2 % num1;
+        const  equalto  = num1==num2;
+        const  not_equalto  = num1 != num2;
+        const  equal2  = num1 === num2;
+        const  greaterThan = num1 > num2;
+        const  lessThan = num1< num2;
+        const  greater_or_eqalto = num1 >= num2;
+        const  less_equal_than = num1 <=  num2;
+        
+        console.log( "78 + 9 =", addition );
+        console.log( "9 - 78  =" , subtraction );
+        console.log( "78 * 9 =", multiplication );
+        console.log( "9 / 78 =",division);
+        console.log( "78 power 2  =",power);
+        console.log( "78 / 9 reminder =",modulo);
+        console.log( "78 == 9 =" , equalto);
+        console.log( "78 != 9 =" , not_equalto);
+        console.log( "78 === 9 =" , equal2);
+        console.log( "78 > 9 =", greaterThan);
+        console.log( "78 < 9 =", lessThan);
+        console.log( "78 >= 9 =", greater_or_eqalto);
+        console.log( "78 <= 9 =", less_equal_than);
+        }
+      
+        
+        // {4}
+console.log(4)
+        
+        {                    
+            const  num1:number = 22;
+            const  num2 :number = 65;
+            const  addition  = num1 + num2;
+            const  subtraction = num2 - num1;
+            const  multiplication  = num1 * num2;
+            const  division = num2 / num1;
+            const  power = num1 ** 2;
+            const  modulo = num2 % num1;
+            const  equalto  = num1==num2;
+            const  not_equalto  = num1 != num2;
+            const  equal2  = num1 === num2;
+            const  greaterThan = num1 > num2;
+            const  lessThan = num1< num2;
+            const  greater_or_eqalto = num1 >= num2;
+            const  less_equal_than = num1 <=  num2;
+            
+            console.log( "22 + 65 =", addition );
+            console.log( "65 - 22  =" , subtraction );
+            console.log( "22 * 65 =", multiplication );
+            console.log( "65 / 22 =",division);
+            console.log( "22 power 2  =",power);
+            console.log( "22 / 65 reminder =",modulo);
+            console.log( "22 == 65 =" , equalto);
+            console.log( "22 != 65 =" , not_equalto);
+            console.log( "22 === 65 =" , equal2);
+            console.log( "22 > 65 =", greaterThan);
+            console.log( "22 < 65 =", lessThan);
+            console.log( "22 >= 65 =", greater_or_eqalto);
+            console.log( "22 <= 65 =", less_equal_than);
+            }
+            
+        //   {5}
+            console.log(5)
+
+            {                    
+                const  num1:string = "a";
+                const  num2 :string = "b";
+                const  addition  = num1 + num2;
+                const  subtraction = null;
+                const  multiplication  = null;
+                const  division = null;
+                const  power = null;
+                const  modulo = null;
+                const  equalto  = num1==num2;
+                const  not_equalto  = num1 != num2;
+                const  equal2  = num1 === num2;
+                const  greaterThan = num1 > num2;
+                const  lessThan = num1< num2;
+                const  greater_or_eqalto = num1 >= num2;
+                const  less_equal_than = num1 <=  num2;
+                
+                console.log( "a + b =", addition );
+                console.log( "b - a =" , subtraction );
+                console.log( "a * b =", multiplication );
+                console.log( "b / a =",division);
+                console.log( "a power 2  =",power);
+                console.log( "a / b reminder =",modulo);
+                console.log( "a == b =" , equalto);
+                console.log( "a != b =" , not_equalto);
+                console.log( "a === b =" , equal2);
+                console.log( "a > b =", greaterThan);
+                console.log( "a < b =", lessThan);
+                console.log( "a >= b =", greater_or_eqalto);
+                console.log( "a <= b =", less_equal_than);
+                }
+                
+                // {6}
+                console.log(6)
+
+
+                {                    
+                    const  num1:string= "waseem";
+                    const  num2 : string = "naseer";
+                    const  addition  = num1 + num2;
+                    const  subtraction = null;
+                    const  multiplication  = null;
+                    const  division = null;
+                    const  power = null;
+                    const  modulo = null;
+                    const  equalto  = num1==num2;
+                    const  not_equalto  = num1 != num2;
+                    const  equal2  = num1 === num2;
+                    const  greaterThan = num1 > num2;
+                    const  lessThan = num1< num2;
+                    const  greater_or_eqalto = num1 >= num2;
+                    const  less_equal_than = num1 <=  num2;
+                    
+                    console.log( "waseem + naseer =", addition );
+                    console.log( " naseer - waseem  =" , subtraction );
+                    console.log( "waseem * naseer =", multiplication );
+                    console.log( "65 / 13 =",division);
+                    console.log( "waseem power 2  =",power);
+                    console.log( "waseem / naseer reminder =",modulo);
+                    console.log( "waseem == naseer =" , equalto);
+                    console.log( "waseem != naseer =" , not_equalto);
+                    console.log( "waseem === naseer =" , equal2);
+                    console.log( "waseem > naseer =", greaterThan);
+                    console.log( "waseem < naseer =", lessThan);
+                    console.log( "waseem >= naseer =", greater_or_eqalto);
+                    console.log( "waseem <= naseer =", less_equal_than);
+                    }
+                
+                    // {7}
+                    console.log(7)
+                    
+                    {                    
+                        const  num1:number =89;
+                        const  num2 :number = 32;
+                        const  addition  = num1 + num2;
+                        const  subtraction = num2 - num1;
+                        const  multiplication  = num1 * num2;
+                        const  division = num2 / num1;
+                        const  power = num1 ** 2;
+                        const  modulo = num2 % num1;
+                        const  equalto  = num1==num2;
+                        const  not_equalto  = num1 != num2;
+                        const  equal2  = num1 === num2;
+                        const  greaterThan = num1 > num2;
+                        const  lessThan = num1< num2;
+                        const  greater_or_eqalto = num1 >= num2;
+                        const  less_equal_than = num1 <=  num2;
+                        
+                        console.log( "32 + 89 =", addition );
+                        console.log( " 89 - 32  =" , subtraction );
+                        console.log( "32 * 89 =", multiplication );
+                        console.log( "89 / 32 =",division);
+                        console.log( "89 power 2  =",power);
+                        console.log( "89 % 32 =" , modulo);
+                        console.log( "32 != 89 =" , not_equalto);
+                        console.log( "32 === 89 =" , equal2);
+                        console.log( "32 > 89=", greaterThan);
+                        console.log( "32 < 89=", lessThan);
+                        console.log( "32 >= 89 =", greater_or_eqalto);
+                        console.log( "32 <= 89 =", less_equal_than);
+                        }
+                        
+                        // {8}
+
+
+                        console.log(8)
+
+                        {                    
+                            const  num1:number = 8;
+                            const  num2 :number = 11;
+                            const  addition  = num1 + num2;
+                            const  subtraction = num2 - num1;
+                            const  multiplication  = num1 * num2;
+                            const  division = num2 / num1;
+                            const  power = num1 ** 2;
+                            const  modulo = num2 % num1;
+                            const  equalto  = num1==num2;
+                            const  not_equalto  = num1 != num2;
+                            const  equal2  = num1 === num2;
+                            const  greaterThan = num1 > num2;
+                            const  lessThan = num1< num2;
+                            const  greater_or_eqalto = num1 >= num2;
+                            const  less_equal_than = num1 <=  num2;
+                            
+                            console.log( "8 + 11 =", addition );
+                            console.log( "11 - 8  =" , subtraction );
+                            console.log( "8 * 11 =", multiplication );
+                            console.log( "11 / 8 =",division);
+                            console.log( "8 power 2  =",power);
+                            console.log( "11 % 8 reminder =",modulo);
+                            console.log( "8 ==  11 =" , equalto);
+                            console.log( "8 !=  11 =" , not_equalto);
+                            console.log( "8 === 11 =" , equal2);
+                            console.log( "8 >  11=", greaterThan);
+                            console.log( "8 <  11=", lessThan);
+                            console.log( "8 >=  11 =", greater_or_eqalto);
+                            console.log( "8 <=  11 =", less_equal_than);
+                            }
+                            
+                            
+            // {9}
+            console.log( 9 )
+
+            {                    
+
+
+                const  num1:number = 12;
+                const  num2 :number = 12;
+                const  addition  = num1 + num2;
+                const  subtraction = num2 - num1;
+                const  multiplication  = num1 * num2;
+                const  division = num2 / num1;
+                const  power = num1 ** 2;
+                const  modulo = num2 % num1;
+                const  equalto  = num1==num2;
+                const  not_equalto  = num1 != num2;
+                const  equal2  = num1 === num2;
+                const  greaterThan = num1 > num2;
+                const  lessThan = num1< num2;
+                const  greater_or_eqalto = num1 >= num2;
+                const  less_equal_than = num1 <=  num2;
+                
+                console.log( "12 + 12 =", addition );
+                console.log( "12 - 12  =" , subtraction );
+                console.log( "12 * 12 =", multiplication );
+                console.log( "12 / 12 =",division);
+                console.log( "12 power 2  =",power);
+                console.log( "12 / 12 reminder =",modulo);
+                console.log( "12 == 12 =" , equalto);
+                console.log( "12 != 12 =" , not_equalto);
+                console.log( "12 === 12 =" , equal2);
+                console.log( "12 > 12 =", greaterThan);
+                console.log( "12 < 12 =", lessThan);
+                console.log( "12 >= 12 =", greater_or_eqalto);
+                console.log( "12 <= 12 =", less_equal_than);
+                }
+
+
+
+
+                // {10}
+                console.log(10)
+
+                {                    
+                    const  num1:number = 5;
+                    const  num2 :number =10 ;
+                    const  addition  = num1 + num2;
+                    const  subtraction = num2 - num1;
+                    const  multiplication  = num1 * num2;
+                    const  division = num2 / num1;
+                    const  power = num1 ** 2;
+                    const  modulo = num2 % num1;
+                    const  equalto  = num1==num2;
+                    const  not_equalto  = num1 != num2;
+                    const  equal2  = num1 === num2;
+                    const  greaterThan = num1 > num2;
+                    const  lessThan = num1< num2;
+                    const  greater_or_eqalto = num1 >= num2;
+                    const  less_equal_than = num1 <=  num2;
+                    
+                    console.log( "5 + 10 =", addition );
+                    console.log( "10 - 5  =" , subtraction );
+                    console.log( "5 * 10 =", multiplication );
+                    console.log( "10 / 5 =",division);
+                    console.log( " 5 power 2  =",power);
+                    console.log( " 5 / 10 reminder =",modulo);
+                    console.log( " 5 == 10 =" , equalto);
+                    console.log( " 5 != 10 =" , not_equalto);
+                    console.log( " 5 === 10 =" , equal2);
+                    console.log( " 5 > 10 =", greaterThan);
+                    console.log( " 5 < 10 =", lessThan);
+                    console.log( " 5 >= 10 =", greater_or_eqalto);
+                    console.log( " 5 <= 10 =", less_equal_than);
+                    }
+                    
+                    
+                    
+                
+                
+                
